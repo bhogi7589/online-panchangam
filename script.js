55dec09e2a0fdada9abd4926b11e8baa3b9a8b1f
bhogi7589 <83229631+bhogi7589@users.noreply.github.com>
2021-08-12 14:29:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
